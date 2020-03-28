e19d954fe66ce6bcb3f97161793d52d296377f2b
JimLynchCodes <mrdotjim@gmail.com>
2020-03-27 23:10:14 -0400

    add twitter links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import { connect } from 'react-redux';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import Todos from '../components/todos/todos';
-import LoginSection from '../components/login/login-section';
-import { IState } from '../state/createStore';
 import { ITodo } from '../models/todo';
 import '../styles/app.css';
 import EcLogoImage from '../components/ec-logo';
@@ -22,32 +17,14 @@ const IndexPage = ({ todos = [], userId = 0 }: { todos: ITodo[] | undefined, use
     <Layout>
       <SEO title='Home' />
 
+      <div className='m-10'></div>
+
       <div style={pStyle}>
-        <h1 className="mt-8 leading-normal text-4xl">A tribe of awesome people teaching EVERYBODY how to code!</h1>
+        <h1 className="mt-8 leading-normal text-4xl">Awesome people teaching EVERYBODY how to code!</h1>
       </div>
 
       <div className='m-10 rounded-md overflow-hidden max-w-md w-64'>
         <EcLogoImage />
-      </div>
-
-      {/* <LoginSection userId={userId} /> */}
-
-      {/* <Todos todos={todos} /> */}
-
-
-      <div className='m-10'>
-
-        <h1 className='leading-normal'>
-          Check out the EverybodyCodes Slack channel!
-        </h1>
-
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => alert('Jumping to slack channel!')}>
-          <h3 className='m-auto'>
-            Join Us In Slack!
-          </h3>
-        </button>
-
       </div>
 
       <div className='m-10'>
@@ -56,44 +33,75 @@ const IndexPage = ({ todos = [], userId = 0 }: { todos: ITodo[] | undefined, use
           Check out the EverybodyCodes Github organization!
         </h1>
 
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => alert('Jumping to our twitter account!')}>
-          <h3 className='m-auto'>
-            Join Us On Github!
-          </h3>
-        </button>
+        <a href='https://github.com/EverybodyCodes'>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <h3 className='m-auto'>
+              Join Us On Github!
+            </h3>
+          </button>
+        </a>
 
       </div>
-   
+
+      <div className='m-10'>
+
+        <h1 className='leading-normal'>
+          Join the conversations in the EverybodyCodes Slack channel!
+        </h1>
+
+        <a href='https://join.slack.com/t/everybodycodes/shared_invite/zt-d1uo3ra9-kkz4R08kI1Tv6dFNRU5Olw'>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <h3 className='m-auto'>
+              Join Us In Slack!
+            </h3>
+          </button>
+        </a>
+
+      </div>
+
+      <div className='m-10'>
+
+        <h1 className='leading-normal'>
+          Are you on the socials? Follow us on Twitter!
+        </h1>
+
+        <a href='https://twitter.com/Everybody_Codes'>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <h3 className='m-auto'>
+              Follow Us On Twitter!
+            </h3>
+          </button>
+        </a>
+
+      </div>
+
       <div className='m-10'>
 
         <h1 className='leading-normal'>
           Have ideas for making EverybodyCodes better?!
         </h1>
 
-        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => alert('Jumping to the github org!')}>
-          <h3 className='m-auto'>
-            Send Us a Tweet!
+        <div className='m-10'></div>
+
+        <h2 className='leading-normal'>
+          We love new contributors with fresh ideas, so don't be shy!
+        </h2>
+
+        <a href='https://twitter.com/Everybody_Codes'>
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            <h3 className='m-auto'>
+              Send Us a Tweet!
           </h3>
-        </button>
+          </button>
+        </a>
 
       </div>
 
-
-
       <div className='m-10'></div>
-
 
     </Layout>
   );
 };
 
-const mapStateToProps = (state: IState) => {
-  return {
-    todos: state.todosReducer.todos,
-    userId: state.loginReducer.userId,
-  };
-};
 
-export default connect(mapStateToProps)(IndexPage);
+export default IndexPage
